9cfd2a410695f20d86a4dec8fc9eda02e5849d48
Meareg Teferi <mearegbuna@gmail.com>
2025-04-15 11:13:46 -0700

    a serializer for the Article model in the article app

--- backend/article/serializers.py
+++ backend/article/serializers.py
@@ -10,4 +10,26 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'name', 'description']
 
+
+   
+class ArticleSerializer(serializers.ModelSerializer):  
+    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), default=serializers.CurrentUserDefault())
+    content_preview = serializers.SerializerMethodField()
+    content = serializers.CharField()
+    category = CategorySerializer(read_only=True)
+    author_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'content', 'author', 'created_date', 'image', 'status', 'content_preview', 'content', 'category', 'author_name']
         
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+    def get_content_preview(self, obj):
+        words = obj.content.split()
+        return ' '.join(words[:20])
+    
+    def get_author_name(self, obj):
+        return obj.author.full_name

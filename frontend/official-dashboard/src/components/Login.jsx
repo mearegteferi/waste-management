73b6c3710cbf47b4d682ef1265ba8232bb33ba32
Meareg Teferi <mearegbuna@gmail.com>
2025-04-26 07:28:49 -0700

    installed react-hook-form for handling form data

--- frontend/official-dashboard/src/components/Login.jsx
+++ frontend/official-dashboard/src/components/Login.jsx
@@ -1,57 +1,7 @@
-import { useState } from "react";
-
 function Login(){
 
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = () => {
-    console.log("submission")
-  }
-  
-    return(
-        <>
-        <div>
-            <h1>Login</h1>
-        </div>
-        <form onSubmit={handleSubmit} className="">
-          <div>
-            <label className="">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className=""
-            />
-          </div>
-
-          <div>
-            <label className="text-red-600">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className=""
-            />
-            </div>
-
-            <button
-                type="submit"
-                className=""
-              >
-                Login
-              </button>
-        </form>
-        </>
-    )
+  return(
+    <>
+    </>
+  )
 }
-
-export default Login;

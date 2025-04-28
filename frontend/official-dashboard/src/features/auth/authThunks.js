3638a5d1685c4048c4171c61c3786bb77318a7cd
Meareg Teferi <mearegbuna@gmail.com>
2025-04-28 12:42:18 -0700

    created a placeholder for a fetch user action created using createAsyncThunk funciton of redux tool kit

--- frontend/official-dashboard/src/features/auth/authThunks.js
+++ frontend/official-dashboard/src/features/auth/authThunks.js
@@ -1,5 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const login = createAsyncThunk('auth/login', async(Credentials, hunkAPI) => {
+export const login = createAsyncThunk('auth/login', async(Credentials, thunkAPI) => {
  
+})
+
+export const fetchUser = createAsyncThunk('auth/fetch', async(_, thunkAPI) => {
+    
 })

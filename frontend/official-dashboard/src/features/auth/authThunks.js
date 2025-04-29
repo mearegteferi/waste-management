244a66b5987cfeb530d27bcd8cc058d5a5eaca30
Meareg Teferi <mearegbuna@gmail.com>
2025-04-29 11:15:47 -0700

    added a loginAPI function to the authAPI file for making login api request to backend

--- frontend/official-dashboard/src/features/auth/authThunks.js
+++ frontend/official-dashboard/src/features/auth/authThunks.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const login = createAsyncThunk('auth/login', async(Credentials, thunkAPI) => {
- 
+ return loginAPI()
 })
 
 export const fetchUser = createAsyncThunk('auth/fetch', async(_, thunkAPI) => {

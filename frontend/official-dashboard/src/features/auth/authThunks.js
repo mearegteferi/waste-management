015f1c6e16fea9df19b2e65a42c3cf05dec78243
Meareg Teferi <mearegbuna@gmail.com>
2025-04-29 11:20:22 -0700

    a function call to the authAPI functions from the authThunk file

--- frontend/official-dashboard/src/features/auth/authThunks.js
+++ frontend/official-dashboard/src/features/auth/authThunks.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const login = createAsyncThunk('auth/login', async(Credentials, thunkAPI) => {
- return loginAPI()
+    return loginAPI()
 })
 
 export const fetchUser = createAsyncThunk('auth/fetch', async(_, thunkAPI) => {
-
+    return fetchAPI()
 })
 
 export const logout = createAsyncThunk('auth/logout', async(_, thunkAPI) => {
-    
+    return logoutAPI()
 })

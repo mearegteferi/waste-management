5ae2a4b96e80fd5f6d0ba825c43231e58af2f499
Meareg Teferi <mearegbuna@gmail.com>
2025-04-29 11:39:38 -0700

    edited the calls to authAPI from the authThunk

--- frontend/official-dashboard/src/features/auth/authThunks.js
+++ frontend/official-dashboard/src/features/auth/authThunks.js
@@ -1,13 +1,35 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { loginAPI, fetchAPI, logoutAPI } from "./authAPI";
 
-export const login = createAsyncThunk('auth/login', async(Credentials, thunkAPI) => {
-    return loginAPI()
-})
 
-export const fetchUser = createAsyncThunk('auth/fetch', async(_, thunkAPI) => {
-    return fetchAPI()
-})
+export const login = createAsyncThunk(
+  "auth/login",
+  async (Credentials, thunkAPI) => {
+    try {
+      const response = await loginAPI(credentials);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-export const logout = createAsyncThunk('auth/logout', async(_, thunkAPI) => {
-    return logoutAPI()
-})
+
+export const fetchUser = createAsyncThunk("auth/fetch", async (_, thunkAPI) => {
+    try {
+        const response = await fetchAPI(credentials);
+        return response;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+});
+
+
+export const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+    try {
+        const response = await logoutAPI(credentials);
+        return response;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+});

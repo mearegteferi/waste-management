0b95fe63e727793c089e5adaf33ef989f0c8a3be
Meareg Teferi <mearegbuna@gmail.com>
2025-04-30 12:59:21 -0700

    a role meta class inside the user model for listing different user roles

--- backend/account/models.py
+++ backend/account/models.py
@@ -3,6 +3,20 @@ from django.db import models
 
 
 class User(AbstractUser):
+    class Role(models.TextChoices):
+      ADMIN = "admin", "Top General Admin"
+      SUBCITY_ADMIN = "subcity_admin", "Subcity Admin"
+      UNION = "union", "Union Representative"
+      EVALUATOR = "evaluator", "Evaluator"
+      DATA_ENCODER = "data_encoder", "Data Encoder"
+      SUBCITY_STAFF = "subcity_staff", "Subcity Staff"
+      TABYA_STAFF = "tabya_staff", "Tabya Staff"
+      RESIDENT = "resident", "Resident"
+      ORGANIZATION = "organization", "Organization"
+      DRIVER = "driver", "Driver"
+      INSPECTOR = "inspector", "Inspector"
+      FINANCE = "finance", "Finance Officer"
+      IT_SUPPORT = "it_support", "IT Support"
     phone = models.CharField(max_length=20, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
 

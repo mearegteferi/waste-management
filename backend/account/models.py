083c6e5ee1bdb8bfa84248e1576383dd734cdfbb
Meareg Teferi <mearegbuna@gmail.com>
2025-04-30 11:54:59 -0700

    added phone and is_verified field to the user model

--- backend/account/models.py
+++ backend/account/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)

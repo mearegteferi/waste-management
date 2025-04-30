a0f813c96eff869e00126cb25b00ed138fa68e5b
Meareg Teferi <mearegbuna@gmail.com>
2025-04-30 11:56:31 -0700

    included a __str__ method for decent model instance output

--- backend/account/models.py
+++ backend/account/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class User(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+      return f"{self.username} ({self.role})"

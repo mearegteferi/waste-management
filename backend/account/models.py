3a1d9ad1d89a30fe2f8ca822d9a0494b9cd552d3
Meareg Teferi <mearegbuna@gmail.com>
2025-05-05 12:17:06 -0700

    created a role model for containing different role models

--- backend/account/models.py
+++ backend/account/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class User(AbstractUser):
 
     role = models.CharField(max_length=30, choices=Role.choices)

701d57ac21a5b79f89e455d9e84bb47bbeda8d7a
Meareg Teferi <mearegbuna@gmail.com>
2025-05-10 11:18:15 -0700

    imported Sidebar and Header to the app.jsx file

--- frontend/official-dashboard/src/App.jsx
+++ frontend/official-dashboard/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Login from "./components/Login"
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
 

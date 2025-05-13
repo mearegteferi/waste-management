a25e7d912b7394fcfadb3e75da24d6b70cba2548
Meareg Teferi <mearegbuna@gmail.com>
2025-05-13 10:45:40 -0700

    a function named create_user_roles is added to the signal file with annotation fo the receiver taking post_migrate (imported previously) as an argument

--- backend/account/signals.py
+++ backend/account/signals.py
@@ -1,3 +1,8 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
+
+
+@receiver(post_migrate)
+def create_user_roles(sender, **kwargs):
+    pass

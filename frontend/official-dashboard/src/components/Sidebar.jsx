dc225d65bd80c0b32fc567d284a88df98f54a8d6
Meareg Teferi <mearegbuna@gmail.com>
2025-05-14 11:02:45 -0700

    added tailwindcss style to the Profile component

--- frontend/official-dashboard/src/components/Sidebar.jsx
+++ frontend/official-dashboard/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 function Sidebar() {
   return (
     <div>
-      <h1>this is the sidebar menu</h1>
+      <h1 >this is the sidebar menu</h1>
     </div>
   );
 }
--- frontend/official-dashboard/src/components/profile.jsx
+++ frontend/official-dashboard/src/components/profile.jsx
@@ -1,7 +1,7 @@
 function Profile() {
   return (
     <div>
-      <h1>user profile</h1>
+      <h1 className="font-bold text-2xl">user profile</h1>
     </div>
   );
 }

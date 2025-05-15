be6917e29aa0998abb8d5a258568ff8aa0c1fec8
Meareg Teferi <mearegbuna@gmail.com>
2025-05-15 11:02:10 -0700

    added some style to the links in the topbar

--- frontend/official-dashboard/src/components/Topbar.jsx
+++ frontend/official-dashboard/src/components/Topbar.jsx
@@ -2,9 +2,9 @@ function Topbar() {
   return (
     <div>
       <h1 className="flex font-bold text-2xl">topbar header</h1>
-      <div>notification</div>
-      <div>setting</div>
-      <div>help</div>
+      <div className="text-amber-300">notification</div>
+      <div className="text-amber-300">setting</div>
+      <div className="text-amber-300">help</div>
     </div>
   );
 }

daf2bc0db074caaf8e467486c4e064e6c87c3831
Meareg Teferi <mearegbuna@gmail.com>
2025-05-15 10:59:15 -0700

    add hyper links to the topbar component

--- frontend/official-dashboard/src/components/Topbar.jsx
+++ frontend/official-dashboard/src/components/Topbar.jsx
@@ -1,7 +1,10 @@
 function Topbar() {
   return (
     <div>
-      <h1 className="font-bold text-2xl">topbar header</h1>
+      <h1 className="flex font-bold text-2xl">topbar header</h1>
+      <div>notification</div>
+      <div>setting</div>
+      <div>help</div>
     </div>
   );
 }

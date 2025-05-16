6d3f6ccbaa9726a37b12f2c76a7a9faa27341852
Meareg Teferi <mearegbuna@gmail.com>
2025-05-16 11:41:01 -0700

    styled the notification button of the topbar component with tailwindcss

--- frontend/official-dashboard/src/components/Topbar.jsx
+++ frontend/official-dashboard/src/components/Topbar.jsx
@@ -2,7 +2,10 @@ function Topbar() {
   return (
     <div>
       <h1 className="flex font-bold text-2xl">topbar header</h1>
-      <div className="text-amber-300">notification</div>
+      <div className="flex items-center cursor-pointer hover:text-amber-600 transition">
+        <span className="material-symbols-outlined mr-1">notifications</span>
+        <span className="hidden sm:inline">Notification</span>
+      </div>
       <div className="text-amber-300">setting</div>
       <div className="text-amber-300">help</div>
     </div>

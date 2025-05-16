b53bf1af718db1df20baacefcc1918901aeff280
Meareg Teferi <mearegbuna@gmail.com>
2025-05-16 11:43:32 -0700

    styled the header for the topbar component

--- frontend/official-dashboard/src/components/Topbar.jsx
+++ frontend/official-dashboard/src/components/Topbar.jsx
@@ -1,7 +1,7 @@
 function Topbar() {
   return (
     <div>
-      <h1 className="flex font-bold text-2xl">topbar header</h1>
+       <h1 className="text-2xl font-bold text-gray-800">Topbar Header</h1>
       <div className="flex items-center cursor-pointer hover:text-amber-600 transition">
         <span className="material-symbols-outlined mr-1">notifications</span>
         <span className="hidden sm:inline">Notification</span>

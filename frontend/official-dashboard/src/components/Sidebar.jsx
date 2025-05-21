6cb1e9f94bf572353652ee2aa434200ecd819b4e
Meareg Teferi <157679708+mearegteferi@users.noreply.github.com>
2025-05-21 11:37:16 -0700

    updated styling of the Sidebar header

--- frontend/official-dashboard/src/components/Sidebar.jsx
+++ frontend/official-dashboard/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 function Sidebar() {
   return (
       <div className="h-screen w-64 bg-white shadow-md border-r border-gray-200 p-6">
-      <h1 className="font-bold text-2xl">this is the sidebar menu</h1>
+     <h1 className="font-bold text-2xl text-gray-800 mb-6">Sidebar Menu</h1>
     </div>
   );
 }

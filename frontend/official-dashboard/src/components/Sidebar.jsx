17d043acb8eef15a197099d3c0793903a878f283
Meareg Teferi <157679708+mearegteferi@users.noreply.github.com>
2025-05-21 11:38:31 -0700

    a unordered list container to contain sidebar menu items

--- frontend/official-dashboard/src/components/Sidebar.jsx
+++ frontend/official-dashboard/src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ function Sidebar() {
   return (
       <div className="h-screen w-64 bg-white shadow-md border-r border-gray-200 p-6">
      <h1 className="font-bold text-2xl text-gray-800 mb-6">Sidebar Menu</h1>
+      <ul className="space-y-4 text-gray-700">
+      </ul>
     </div>
   );
 }

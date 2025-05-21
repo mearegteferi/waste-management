5158b746e861f685f4c433ad1a283dcd7a6ed2c8
Meareg Teferi <157679708+mearegteferi@users.noreply.github.com>
2025-05-21 11:40:09 -0700

    logout menu item for logging user out

--- frontend/official-dashboard/src/components/Sidebar.jsx
+++ frontend/official-dashboard/src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ function Sidebar() {
      <h1 className="font-bold text-2xl text-gray-800 mb-6">Sidebar Menu</h1>
       <ul className="space-y-4 text-gray-700">
         <li className="cursor-pointer hover:text-amber-600 transition">Dashboard</li>
+        <li className="cursor-pointer hover:text-amber-600 transition">Logout</li>
       </ul>
     </div>
   );

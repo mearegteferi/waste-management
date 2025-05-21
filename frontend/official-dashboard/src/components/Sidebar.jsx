b56bce8ec8a0a2d8773a9bfbaca7721893a8f1d4
Meareg Teferi <157679708+mearegteferi@users.noreply.github.com>
2025-05-21 11:36:25 -0700

    added some tailwindcss styling to the container div of Sidebar component

--- frontend/official-dashboard/src/components/Sidebar.jsx
+++ frontend/official-dashboard/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 function Sidebar() {
   return (
-    <div>
+      <div className="h-screen w-64 bg-white shadow-md border-r border-gray-200 p-6">
       <h1 className="font-bold text-2xl">this is the sidebar menu</h1>
     </div>
   );
